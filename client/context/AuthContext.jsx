80dcef1a56a89e123a25b8bda6cb934d4166626b
Fatima Badaoui <fatimina615@gmail.com>
2025-06-12 20:29:06 +0200

    add userContext

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,71 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  // check if user is logged in from the cookie
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = async (username, password) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/users/login",
+        {
+          username,
+          password,
+        }
+      );
+      const userData = response.data;
+      setUser(userData);
+      localStorage.setItem("user", JSON.stringify(userData));
+      console.log("Login successful:", userData);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
+  const signup = async (username, firstName, lastName, password) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/users/signup",
+        {
+          username,
+          firstName,
+          lastName,
+          password,
+        }
+      );
+      const userData = response.data;
+      setUser(userData);
+      localStorage.setItem("user", JSON.stringify(userData));
+      console.log("Signup successful:", userData);
+    } catch (error) {
+      console.error("Signup failed:", error);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    console.log("User logged out");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, signup, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
