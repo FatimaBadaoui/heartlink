986e4423748ecaf97044585b4531eb21b105706d
Fatima Badaoui <fatimina615@gmail.com>
2025-07-08 23:24:13 +0200

    start backend request

--- client/pages/EditProfile.jsx
+++ client/pages/EditProfile.jsx
@@ -1,17 +1,81 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext.jsx";
 import { BsEye, BsEyeSlash } from "react-icons/bs";
+import axios from "axios";
+import serverUrl from "../urls.js";
+import { useNavigate } from "react-router";
 
 const EditProfile = () => {
   const { user } = useAuth();
-  console.log("user in EditProfile:", user);
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
   const [avatar, setAvatar] = useState(user.avatar);
+  const [avatarShown, setAvatarShown] = useState(user.avatar);
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== repeatPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    console.log("avatar:", avatar);
+    // Convert image to base64
+    const toBase64 = (file) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+
+    const base64Image = await toBase64(avatar);
+
+    console.log("base64Image:", base64Image);
+    try {
+      if (password) {
+        await axios.put(
+          `${serverUrl}/api/users`,
+          {
+            firstName,
+            lastName,
+            avatar: base64Image,
+            password,
+          },
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      } else {
+        await axios.put(
+          `${serverUrl}/api/users`,
+          {
+            firstName,
+            lastName,
+            avatar: base64Image,
+          },
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+      navigate("/");
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      alert("Failed to update profile");
+    }
+  };
 
   return (
     <div className="w-full mx-auto p-4 max-w-3xl">
@@ -19,7 +83,7 @@ const EditProfile = () => {
       <div className="flex flex-col md:flex-row items-center justify-between w-full">
         <div className="flex flex-col gap-2 w-[30%] p-4 items-center">
           <img
-            src={avatar}
+            src={avatarShown}
             alt="user avatar"
             className="object-cover rounded-full h-52 w-52 mb-4"
           />
@@ -28,7 +92,10 @@ const EditProfile = () => {
             accept="image/*"
             className="hidden"
             id="file-input"
-            onChange={(e) => setAvatar(URL.createObjectURL(e.target.files[0]))}
+            onChange={(e) => {
+              setAvatarShown(URL.createObjectURL(e.target.files[0]));
+              setAvatar(e.target.files[0]);
+            }}
           />
           <label
             htmlFor="file-input"
@@ -96,7 +163,10 @@ const EditProfile = () => {
               />
             )}
           </div>
-          <button className="bg-white text-black rounded py-1 cursor-pointer hover:bg-purple-400 transition-colors duration-300">
+          <button
+            className="bg-white text-black rounded py-1 cursor-pointer hover:bg-purple-400 transition-colors duration-300"
+            onClick={handleSubmit}
+          >
             Save Changes
           </button>
         </div>
